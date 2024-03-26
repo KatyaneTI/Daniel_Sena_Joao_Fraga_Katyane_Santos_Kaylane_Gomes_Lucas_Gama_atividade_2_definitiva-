f3ca7ada734620418f2d1aae16f8c4ee90f4d3b6
joaoufs23 <142043624+joaoufs23@users.noreply.github.com>
2024-03-25 22:33:12 -0300

    Update main.java

---
+++ src/main.java
@@ -0,0 +1,13 @@
+package ES2;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        //Scanner scanner = new Scanner(System.in);
+    	String[] opcoes = {"Competência concessão", "Espécie", "CID", "CID", "Despacho", "Dt Nascimento", "Sexo.", "Clientela", "Mun Resid", "Vínculo dependentes", "", "", "", ""};
+        String Coluna = "UF";
+        String valor = "Alagoas";
+        Tabela requisicao = new Tabela(Coluna, valor);
+        
+    }
+}
