0959732e4063a81fb247ad9afef97c97ea85e035
LucasGamaV <114373261+LucasGamaV@users.noreply.github.com>
2024-03-25 22:11:51 -0300

    Update Observador.java
    Interface do observador

--- Observador.java
+++ Observador.java
@@ -5,14 +5,3 @@ import java.util.List;
 interface Observador {
     void atualizar(List<String> dados);
 }
-
-// Classe Tabela que atua como Observador
-class Tabela implements Observador {
-    @Override
-    public void atualizar(List<String> dados) {
-        System.out.println("Tabela: Dados atualizados:");
-        for (String dado : dados) {
-            System.out.println(dado);
-        }
-    }
-}
