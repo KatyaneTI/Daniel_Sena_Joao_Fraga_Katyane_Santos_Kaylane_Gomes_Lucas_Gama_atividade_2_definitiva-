51c5589f67d2481f9c7fa7cd50a19b2d73f025d8
Kaylanevitoriagomes <kaylane@academico.ufs.br>
2024-03-26 16:16:36 -0300

    Create Facade.java

---
+++ Facade.java
@@ -0,0 +1,39 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Facade {
+
+    public void iniciarProcesso() {
+        gerarEstatisticas();
+        gerarRelatorioJSON();
+        visualizarBeneficios();
+    }
+
+    private void gerarEstatisticas() {
+        Estatistica estatistica = new Estatistica();
+        estatistica.gerarEstatistica();
+    }
+
+    private void gerarRelatorioJSON() {
+        Relatorio relatorio = new Relatorio();
+        relatorio.setTipo("json");
+        relatorio.gerarRelatorio();
+    }
+
+    private void gerarRelatorioCSV() {
+        Relatorio relatorio = new Relatorio();
+        relatorio.setTipo("csv");
+        relatorio.gerarRelatorio();
+    }
+
+    private void visualizarBeneficios() {
+        Beneficio beneficio = new Beneficio();
+        beneficio.visualizarBeneficios();
+    }
+
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.iniciarProcesso();
+    }
+}
