d2673efaa1a7bc6ea4f23abb5671af722caf29c4
Kaylanevitoriagomes <kaylane@academico.ufs.br>
2024-03-26 16:17:44 -0300

    update

--- Facade.java
+++ Facade.java
@@ -7,6 +7,7 @@ public class Facade {
     public void iniciarProcesso() {
         gerarEstatisticas();
         gerarRelatorioJSON();
+        gerarRelatorioCSV();
         visualizarBeneficios();
     }
 
