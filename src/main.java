aff8f1e64c284048c415509d2956fedf94563c95
joaoufs23 <142043624+joaoufs23@users.noreply.github.com>
2024-03-25 22:35:00 -0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         //Scanner scanner = new Scanner(System.in);
-    	String[] opcoes = {"Competência concessão", "Espécie", "CID", "CID", "Despacho", "Dt Nascimento", "Sexo.", "Clientela", "Mun Resid", "Vínculo dependentes", "", "", "", ""};
+    	String[] opcoes = {"Competência concessão", "Espécie", "CID", "CID", "Despacho", "Dt Nascimento", "Sexo.", "Clientela", "Mun Resid", "Vínculo dependentes", "Forma Filiação", "UF", "Qt SM RMI"};
         String Coluna = "UF";
         String valor = "Alagoas";
         Tabela requisicao = new Tabela(Coluna, valor);
