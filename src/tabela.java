714d88cbfbd8049cfcf3ff8f69adb404fa0c0692
joaoufs23 <142043624+joaoufs23@users.noreply.github.com>
2024-03-25 22:33:39 -0300

    Update tabela.java

---
+++ src/tabela.java
@@ -0,0 +1,13 @@
+package ES2;
+
+public class Tabela {
+	private String Coluna;
+	private String Valor;
+	
+	public Tabela(String coluna, String valor) {
+        this.Coluna = coluna;
+        this.Valor = valor;
+    }
+	String[] meuArray;
+	
+}
