c091b6433da6493dbc552c1c9a17be195c5bcf1e
Kaylanevitoriagomes <kaylane@academico.ufs.br>
2024-03-26 12:50:38 -0300

    update

---
+++ Relatorio.java
@@ -0,0 +1,25 @@
+public class Relatorio {
+    private int id;
+    private String tipo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void gerarRelatorio() {
+        System.out.println("Gerando relatório");
+      
+    }
+}
