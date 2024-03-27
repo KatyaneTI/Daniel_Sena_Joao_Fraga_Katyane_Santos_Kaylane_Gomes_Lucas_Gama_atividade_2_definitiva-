aa2f2897879d3272eb92da1fed9cef37c794b78f
Kaylanevitoriagomes <kaylane@academico.ufs.br>
2024-03-26 22:31:16 -0300

    update

---
+++ src/Estatistica.java
@@ -0,0 +1,26 @@
+package src;
+
+public class Estatistica {
+    private int id;
+    private int quantidade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public void gerarEstatistica() {
+        System.out.println("Gerando estatística");
+    }
+}
