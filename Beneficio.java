5972d72db6638888527b28fd7b6dcbf830a8d7a5
Kaylanevitoriagomes <kaylane@academico.ufs.br>
2024-03-31 11:58:14 -0300

    update

--- Beneficio.java
+++ Beneficio.java
@@ -1,179 +1,179 @@
 import java.sql.*;
-import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
 public class Beneficio {
-    private int id;
-    private String competenciaConcessao;
-    private String especie;
-    private String cid;
-    private String cidEspecificacao;
-    private String despacho;
-    private Date dtNascimento;
-    private String sexo;
-    private String clientela;
-    private String munResid;
-    private String vinculoDependentes;
-    private String formaFiliacao;
-    private String uf;
-    private double qtSmRmi;
+    private String dbUrl;
+    private String user;
+    private String pass;
 
-
-    
-    public int getId() {
-        return id;
+    public Beneficio(String dbUrl, String user, String pass) {
+        this.dbUrl = dbUrl;
+        this.user = user;
+        this.pass = pass;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public void readBeneficio() {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = DriverManager.getConnection(dbUrl, user, pass);
+            stmt = conn.createStatement();
+            String sql = "SELECT * FROM BENEFICIO";
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String competenciaConcessao = rs.getString("competencia_concessao");
+                String especie = rs.getString("especie");
+                String cid = rs.getString("cid");
+                String cidEspecificacao = rs.getString("cid_especificacao");
+                String despacho = rs.getString("despacho");
+                Date dtNascimento = rs.getDate("dt_nascimento");
+                String sexo = rs.getString("sexo");
+                String clientela = rs.getString("clientela");
+                String munResid = rs.getString("mun_resid");
+                String vinculoDependentes = rs.getString("vinculo_dependentes");
+                String formaFiliacao = rs.getString("forma_filiacao");
+                String uf = rs.getString("uf");
+                String qtSmRmi = rs.getString("qt_sm_rmi");
 
-    public String getCompetenciaConcessao() {
-        return competenciaConcessao;
-    }
-
-    public void setCompetenciaConcessao(String competenciaConcessao) {
-        this.competenciaConcessao = competenciaConcessao;
-    }
-
-    public String getEspecie() {
-        return especie;
-    }
-
-    public void setEspecie(String especie) {
-        this.especie = especie;
-    }
-
-    public String getCid() {
-        return cid;
-    }
-
-    public void setCid(String cid) {
-        this.cid = cid;
-    }
-
-    public String getCidEspecificacao() {
-        return cidEspecificacao;
-    }
-
-    public void setCidEspecificacao(String cidEspecificacao) {
-        this.cidEspecificacao = cidEspecificacao;
-    }
-
-    public String getDespacho() {
-        return despacho;
-    }
-
-    public void setDespacho(String despacho) {
-        this.despacho = despacho;
-    }
-
-    public Date getDtNascimento() {
-        return dtNascimento;
-    }
-
-    public void setDtNascimento(Date dtNascimento) {
-        this.dtNascimento = dtNascimento;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
-
-    public String getClientela() {
-        return clientela;
-    }
-
-    public void setClientela(String clientela) {
-        this.clientela = clientela;
-    }
-
-    public String getMunResid() {
-        return munResid;
-    }
-
-    public void setMunResid(String munResid) {
-        this.munResid = munResid;
-    }
-
-    public String getVinculoDependentes() {
-        return vinculoDependentes;
-    }
-
-    public void setVinculoDependentes(String vinculoDependentes) {
-        this.vinculoDependentes = vinculoDependentes;
-    }
-
-    public String getFormaFiliacao() {
-        return formaFiliacao;
-    }
-
-    public void setFormaFiliacao(String formaFiliacao) {
-        this.formaFiliacao = formaFiliacao;
-    }
-
-    public String getUf() {
-        return uf;
-    }
-
-    public void setUf(String uf) {
-        this.uf = uf;
-    }
-
-    public double getQtSmRmi() {
-        return qtSmRmi;
-    }
-
-    public void setQtSmRmi(double qtSmRmi) {
-        this.qtSmRmi = qtSmRmi;
-    }
-}
-private void visualizarBeneficios() {
-    String url = "jdbc:postgresql://bd.c90644aq4xwd.us-east-1.rds.amazonaws.com:5432/bd";
-    String user = "postgres";
-    String password = "bancodedados";
-
-    try (Connection conn = DriverManager.getConnection(url, user, password)) {
-        String sql = "SELECT * FROM Beneficio";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-            try (ResultSet rs = stmt.executeQuery()) {
-                while (rs.next()) {
-                    int id = rs.getInt("id");
-                    String competenciaConcessao = rs.getString("competencia_concessao");
-                    String especie = rs.getString("especie");
-                    String cid = rs.getString("cid");
-                    String cidEspecificacao = rs.getString("cid_especificacao");
-                    String despacho = rs.getString("despacho");
-                    Date dtNascimento = rs.getDate("dt_Nascimento");
-                    String sexo = rs.getString("sexo");
-                    String clientela = rs.getString("clientela");
-                    String munResid = rs.getString("mun_Resid");
-                    String vinculoDependentes = rs.getString("vinculo_dependentes");
-                    String formaFiliacao = rs.getString("forma_filiacao");
-                    String uf = rs.getString("uf");
-                    double qtSmRmi = rs.getDouble("qt_sm_rmi");
-
-                    System.out.println("Benefício " + id + ":");
-                    System.out.println("Competência de Concessão: " + competenciaConcessao);
-                    System.out.println("Espécie: " + especie);
-                    System.out.println("CID: " + cid);
-                    System.out.println("CID Especificação: " + cidEspecificacao);
-                    System.out.println("Despacho: " + despacho);
-                    System.out.println("Data de Nascimento: " + dtNascimento);
-                    System.out.println("Sexo: " + sexo);
-                    System.out.println("Clientela: " + clientela);
-                    System.out.println("Município de Residência: " + munResid);
-                    System.out.println("Vínculo Dependentes: " + vinculoDependentes);
-                    System.out.println("Forma de Filiação: " + formaFiliacao);
-                    System.out.println("UF: " + uf);
-                    System.out.println("Qt. SM RMI: " + qtSmRmi);
-                }
+                System.out.println("ID: " + id);
+                System.out.println("Competência de Concessão: " + competenciaConcessao);
+                System.out.println("Espécie: " + especie);
+                System.out.println("CID: " + cid);
+                System.out.println("CID Especificação: " + cidEspecificacao);
+                System.out.println("Despacho: " + despacho);
+                System.out.println("Data de Nascimento: " + dtNascimento);
+                System.out.println("Sexo: " + sexo);
+                System.out.println("Clientela: " + clientela);
+                System.out.println("Município de Residência: " + munResid);
+                System.out.println("Vínculo Dependentes: " + vinculoDependentes);
+                System.out.println("Forma de Filiação: " + formaFiliacao);
+                System.out.println("UF: " + uf);
+                System.out.println("Quantidade de SM RMI: " + qtSmRmi);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) stmt.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
-    } catch (SQLException e) {
-        e.printStackTrace();
+    }
+
+    public void createBeneficio(Scanner scanner) throws ParseException {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+            conn = DriverManager.getConnection(dbUrl, user, pass);
+            String sql = "INSERT INTO BENEFICIO (competencia_concessao, especie, cid, cid_especificacao, despacho, dt_nascimento, sexo, clientela, mun_resid, vinculo_dependentes, forma_filiacao, uf, qt_sm_rmi) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            pstmt = conn.prepareStatement(sql);
+
+            System.out.print("Competência de Concessão: ");
+            pstmt.setString(1, scanner.nextLine());
+            System.out.print("Espécie: ");
+            pstmt.setString(2, scanner.nextLine());
+            System.out.print("CID: ");
+            pstmt.setString(3, scanner.nextLine());
+            System.out.print("CID Especificação: ");
+            pstmt.setString(4, scanner.nextLine());
+            System.out.print("Despacho: ");
+            pstmt.setString(5, scanner.nextLine());
+            System.out.print("Data de Nascimento (AAAA-MM-DD): ");
+            String dataNascimentoStr = scanner.nextLine();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            java.util.Date parsedDate = sdf.parse(dataNascimentoStr);
+            pstmt.setDate(6, new java.sql.Date(parsedDate.getTime()));
+            System.out.print("Sexo: ");
+            pstmt.setString(7, scanner.nextLine());
+            System.out.print("Clientela: ");
+            pstmt.setString(8, scanner.nextLine());
+            System.out.print("Município de Residência: ");
+            pstmt.setString(9, scanner.nextLine());
+            System.out.print("Vínculo Dependentes: ");
+            pstmt.setString(10, scanner.nextLine());
+            System.out.print("Forma de Filiação: ");
+            pstmt.setString(11, scanner.nextLine());
+            System.out.print("UF: ");
+            pstmt.setString(12, scanner.nextLine());
+            System.out.print("Quantidade de SM RMI: ");
+            pstmt.setBigDecimal(13, scanner.nextBigDecimal());
+            scanner.nextLine();
+            int rowsInserted = pstmt.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("Novo registro inserido com sucesso.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pstmt != null) pstmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void updateBeneficio(Scanner scanner) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+            conn = DriverManager.getConnection(dbUrl, user, pass);
+            String sql = "UPDATE BENEFICIO SET qt_sm_rmi = ? WHERE id = ?";
+            pstmt = conn.prepareStatement(sql);
+
+            System.out.print("Novo valor da qt_sm_rmi: ");
+            pstmt.setString(1, scanner.nextLine());
+            System.out.print("ID do registro a ser atualizado: ");
+            pstmt.setInt(2, scanner.nextInt());
+            scanner.nextLine();
+
+            int rowsUpdated = pstmt.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("Registro atualizado com sucesso.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pstmt != null) pstmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void deleteBeneficio(Scanner scanner) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+            conn = DriverManager.getConnection(dbUrl, user, pass);
+            String sql = "DELETE FROM BENEFICIO WHERE id = ?";
+            pstmt = conn.prepareStatement(sql);
+
+            System.out.print("ID do registro a ser excluído: ");
+            pstmt.setInt(1, scanner.nextInt());
+            scanner.nextLine();
+            int rowsDeleted = pstmt.executeUpdate();
+            if (rowsDeleted > 0) {
+                System.out.println("Registro excluído com sucesso.");
+            } else {
+                System.out.println("Nenhum registro encontrado para excluir com o ID fornecido.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pstmt != null) pstmt.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
